e5047d4d60ba580252d6bf3b3e76dd51065c5493
FerSaul <fsmoreno@espol.edu.ec>
2018-11-08 12:58:32 -0500

    paquete operciones correcto

---
+++ TallerSOLID/TallerSOLID/src/ProcesoCorrecto/CambiadorLeche.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProcesoCorrecto;
+
+/**
+ *
+ * @author User-PC
+ */
+
+import Leche2.*;
+public class CambiadorLeche {
+    public void cambiarLeche(Leche tipoLeche){ 
+        tipoLeche.cambiar();
+    }
+}
---
+++ TallerSOLID/TallerSOLID/src/ProcesoCorrecto/OperacionesAderezoCorrecto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProcesoCorrecto;
+
+import Otros.Aderezo;
+
+/**
+ *
+ * @author User-PC
+ */
+public interface OperacionesAderezoCorrecto {
+    public void anadirAderezo(Aderezo aderezo);
+    
+    public void quitarAderezo(Aderezo aderezo);
+}
